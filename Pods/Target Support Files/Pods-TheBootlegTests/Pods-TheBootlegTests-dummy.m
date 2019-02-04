798a5a6c144157d188e97057d1d516129fe6fcc4
Marco Aguilera <maaguilera@csumb.edu>
2019-02-04 14:37:23 -0800

    First commit

---
+++ Pods/Target Support Files/Pods-TheBootlegTests/Pods-TheBootlegTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheBootlegTests : NSObject
+@end
+@implementation PodsDummy_Pods_TheBootlegTests
+@end
---
+++ TheBootlegTests/TheBootlegTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheBootlegTests.swift
+//  TheBootlegTests
+//
+//  Created by Marco Aguilera on 2/4/19.
+//  Copyright © 2019 Marco Aguilera. All rights reserved.
+//
+
+import XCTest
+@testable import TheBootleg
+
+class TheBootlegTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
