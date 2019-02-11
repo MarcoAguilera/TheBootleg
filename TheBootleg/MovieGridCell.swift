ac4ba051496e1155787ca2e711e7a4fba8713fc5
Marco Aguilera <maaguilera@csumb.edu>
2019-02-11 09:27:49 -0800

    Assignment 2

---
+++ TheBootleg/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  TheBootleg
+//
+//  Created by Marco Aguilera on 2/11/19.
+//  Copyright © 2019 Marco Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
